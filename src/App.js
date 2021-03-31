8c08530448e550cd9be3ec380bb656042d0e85cf
Suraj <info.blogtheorem@gmail.com>
2021-03-31 17:19:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./components/Main";
+
+import ClassComponent from "./components/Concepts/ClassComponent/ClassComponent";
+import State from "./components/Concepts/State/State";
+import PropsAndEvents from "./components/Concepts/PropsAndEvents/PropsAndEvents";
+import FunctionalComponent from "./components/Concepts/FunctionalComponent/FunctionalComponent";
+import UseState from "./components/Concepts/UseState/UseState";
+import PropsAndEventsFC from "./components/Concepts/PropsAndEventsFC/PropsAndEventsFC";
+import Form from "./components/Concepts/Form/Form";
+import ConditionalRendering from "./components/Concepts/ConditionalRendering/ConditionalRendering";
+import ListAndkeys from "./components/Concepts/ListAndkeys/ListAndkeys";
+import UseEffect from "./components/Concepts/UseEffect/UseEffect";
+import UseRefs from "./components/Concepts/UseRefs/UseRefs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <div className="middle">
+          <Route path="/1">
+            <ClassComponent />
+          </Route>
+          <Route path="/2">
+            <State />
+          </Route>
+          <Route path="/3">
+            <PropsAndEvents />
+          </Route>
+          <Route path="/5">
+            <FunctionalComponent />
+          </Route>
+          <Route path="/6">
+            <UseState />
+          </Route>
+          <Route path="/8">
+            <Form />
+          </Route>
+          <Route path="/9">
+            <ConditionalRendering />
+          </Route>
+          <Route path="/10">
+            <ListAndkeys />
+          </Route>
+          <Route path="/11">
+            <UseEffect />
+          </Route>
+          <Route path="/12">
+            <UseRefs />
+          </Route>
+          {/* <Route path="/12">
+            <CustomHooks />
+          </Route>  */}
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
