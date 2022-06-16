78588d1bc36eb45240ca4c0221f2467ba6e2a9a1
Suraj <info.blogtheorem@gmail.com>
2022-06-16 18:03:27 +0530

    refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,16 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <p>Made with ☕ and 💻 by blogtheorem</p>
+      <p>
+        Made with ☕ and 💻 by{" "}
+        <a
+          style={{ color: "#abd1c6" }}
+          target="__blank"
+          href="http://blogtheorem.com"
+        >
+          blogtheorem
+        </a>{" "}
+      </p>
     </footer>
   );
 }
